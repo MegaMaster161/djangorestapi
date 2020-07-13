0dc3c0b7ab60933a295918aa296a1db1a82ca8b1
Vadim Lazev <megasvdonsk@gmail.com>
2020-07-14 01:25:59 +0300

    Описана модель для работы со статьями v.0.1a

--- api/blog/models.py
+++ api/blog/models.py
@@ -1,15 +1,31 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 #описываем модель статьи
 class Article(models.Model):
+    ACTIVE_CHOICES = (
+        ('draft','Черновик'),
+        ('pub', 'Опубликовано'),
+    )
     title = models.CharField()
     titleimg = models.CharField()
     meta = models.CharField()
-    active = models.IntegerField()
-    body = models.CharField()
-    owner = models.IntegerField()
+    slug = models.SlugField(max_length=250,unique_for_date='createdAt')
+    active = models.CharField(max_length=15, choices=ACTIVE_CHOICES, default='draft')
+    body = models.TextField()
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, 
+                                blank=True, null=True, related_name='blog_posts')
     createdAt = models.DateField()
     updateAt = models.DateField()
+
+    
+    class Meta:
+        ordering = ('-createdAt',)
+
+
+    def __str__(self):
+        return self.title
 
 #описываем модель пользователя
 class Users(models.Model):
